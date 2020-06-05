676aebe9c9cf0974e2a828475e26dbd17b8cd9bd
Dominic Bertocchini <dominicbertocchini@gmail.com>
2020-06-05 07:19:15 -0700

    Added rendering for paddles

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@ use amethyst::{
         RenderingBundle,
     },
     utils::application_root_dir,
+    core::transform::{
+        TransformBundle
+    }
 };
 
-//game state struct
-pub struct Pong;
-impl SimpleState for Pong {}
+//opens file of name "pong.rs" as a module
+mod pong;
+//imports Pong struct
+use crate::pong::Pong;
 
 fn main() -> amethyst::Result<()> {
     //start error logging
@@ -22,8 +26,21 @@ fn main() -> amethyst::Result<()> {
     //display config file
     let display_config_path = app_root.join("config").join("display.ron");
 
-    //default game logic setup
-    let game_data = GameDataBuilder::default();
+    //game logic setup
+    let game_data = GameDataBuilder::default()
+    .with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+            .with_plugin(RenderFlat2D::default()),
+    )?
+    .with_bundle(
+        TransformBundle::new()
+    )?;
 
     //assets directory
     let assets_dir = app_root.join("assets");
